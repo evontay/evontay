60f6f87d792d8d74ced0a7b09ff1df58b1dd114b
Evon <fumblies@gmail.com>
2016-08-05 15:43:18 +0800

    added footer contact

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -3,6 +3,17 @@ import Ember from 'ember'
 export default Ember.Component.extend({
   didRender: function () {
     console.log('hello')
-    $('#nav-bar').hide()
+    // $('#nav-bar').hide()
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 500) {
+        $('#nav-bar').fadeIn()
+      }
+
+      else if ($(this) === $('#contact')) {
+        $('#nav-bar').fadeOut()
+      } else {
+        $('#nav-bar').fadeOut()
+      }
+    })
   }
 })
